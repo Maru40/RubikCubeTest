f4f11cb95c878305f71f4b51a2cdc61c0830aafc
Maru40 <umeda.4072@gmail.com>
2023-04-19 09:21:58 +0900

    RubikCubeの更新

--- Assets/02_Scripts/RubikCube/Factory/Auto_RubikCube.cs
+++ Assets/02_Scripts/RubikCube/Factory/Auto_RubikCube.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Security.Cryptography;
+using Unity.Services.Core.Environments;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Auto_RubikCube : MonoBehaviour
 {
@@ -22,7 +25,7 @@ public class Auto_RubikCube : MonoBehaviour
 
     private List<GroupData> mGroupDatas = new List<GroupData>();
 
-    private Dictionary<AxisType, List<GroupData>> mGraupDatasMap = 
+    private Dictionary<AxisType, List<GroupData>> mGraupDatasMap =
         new Dictionary<AxisType, List<GroupData>>();
 
     [SerializeField]
@@ -52,10 +55,51 @@ public class Auto_RubikCube : MonoBehaviour
 
     private AxisType mAxisType = AxisType.kX;
 
+    private List<float> mComparisonSelectValue = new List<float>();
+
+    public enum eOperationType {
+        U,  //↑
+        D,  //下
+        L,
+        R,
+        F,
+        B
+    }
+
+    public class State
+    {
+        //各コーナーパーツの場所を表す8次元ベクトル
+        public int[] cp = new int[8] { 0, 1, 2, 3, 4, 5, 6, 7 };
+        //各コーナーの向きがどの向きを向いているかどうかの情報を表す8次元ベクトル
+        public int[] co = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+        //各エッジパーツの場所を表す12次元ベクトル
+        public int[] ep = new int[12] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        //各エッジパーツの向きを表す12次元ベクトル
+        public int[] eo = new int[12] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    }
+
+    private Dictionary<eOperationType, State> mMoveMap = new Dictionary<eOperationType, State>();
+
+    //ステート管理系
+    State mState;
+
+    ////各コーナーパーツの場所を表す8次元ベクトル
+    //int[] cp = new int[8] { 0, 1, 2, 3, 4, 5, 6, 7 }; 
+    ////各コーナーの向きがどの向きを向いているかどうかの情報を表す8次元ベクトル
+    //int[] co = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+    ////各エッジパーツの場所を表す12次元ベクトル
+    //int[] ep = new int[12] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+    ////各エッジパーツの向きを表す12次元ベクトル
+    //int[] eo = new int[12] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
     private void Start()
     {
         Factory_RubikCubes();   //キューブの生成
-        CreateRotationGroup();
+        CreateRotationGroup();  //グループの作成
+        CreateComparisonSelectValue();
+
+        CreateOperationMoves();
+        ColorAdjust();    //カラー調整
 
         mRotationParent = Instantiate(mRotationParentPrefab).GetComponent<RotationParent>();
     }
@@ -63,6 +107,79 @@ public class Auto_RubikCube : MonoBehaviour
     private void Update()
     {
         InputUpdate();
+    }
+
+    private void CreateOperationMoves()
+    {
+        var state = new State();
+
+        state.cp = new int[8] { 3, 0, 1, 2, 4, 5, 6, 7 };
+        state.co = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+        state.ep = new int[12] { 0, 1, 2, 3, 7, 4, 5, 6, 8, 9, 10, 11 };
+        state.eo = new int[12] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        mMoveMap[eOperationType.U] = state;
+
+        state.cp = new int[8] { 0, 1, 2, 3, 5, 6, 7, 4 };
+        state.co = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+        state.ep = new int[12] { 0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 8 };
+        state.eo = new int[12] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        mMoveMap[eOperationType.D] = state;
+
+        state.cp = new int[8] { 4, 1, 2, 0, 7, 5, 6, 3 };
+        state.co = new int[8] { 2, 0, 0, 1, 1, 0, 0, 2 };
+        state.ep = new int[12] { 11, 1, 2, 7, 4, 5, 6, 0, 8, 9, 10, 3 };
+        state.eo = new int[12] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        mMoveMap[eOperationType.L] = state;
+
+        state.cp = new int[8] { 0, 2, 6, 3, 4, 1, 5, 7 };
+        state.co = new int[8] { 0, 1, 2, 0, 0, 2, 1, 0 };
+        state.ep = new int[12] { 0, 5, 9, 3, 4, 2, 6, 7, 8, 1, 10, 11 };
+        state.eo = new int[12] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        mMoveMap[eOperationType.R] = state;
+
+        state.cp = new int[8] { 0, 1, 3, 7, 4, 5, 2, 6 };
+        state.co = new int[8] { 0, 0, 1, 2, 0, 0, 2, 1 };
+        state.ep = new int[12] { 0, 1, 6, 10, 4, 5, 3, 7, 8, 9, 2, 11 };
+        state.eo = new int[12] { 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0 };
+        mMoveMap[eOperationType.F] = state;
+
+        state.cp = new int[8] { 1, 5, 2, 3, 0, 4, 6, 7 };
+        state.co = new int[8] { 1, 2, 0, 0, 2, 1, 0, 0 };
+        state.ep = new int[12] { 4, 8, 2, 3, 1, 5, 6, 7, 0, 9, 10, 11 };
+        state.eo = new int[12] { 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 };
+        mMoveMap[eOperationType.B] = state;
+    }
+
+    State AddState(State self, State move)
+    {
+        State newState = new State();
+
+        newState.cp = AddCpState(self, move);
+
+        return newState;
+    }
+
+    int[] AddCpState(State self, State move)
+    {
+        var result = new int[8];
+
+        for (int i = 0; i < result.Length; ++i)
+        {
+            int index = move.cp[i];
+            result[i] = self.cp[index];
+        }
+
+        return result;
+    }
+
+    private void ColorAdjust()
+    {
+        var centerPosition = Vector3.zero;
+
+        foreach(var cube in mCubes)
+        {
+            var toCubeVec = cube.transform.position - centerPosition;
+        }
     }
 
     void StartRotation()
@@ -82,12 +199,48 @@ public class Auto_RubikCube : MonoBehaviour
             cube.transform.SetParent(null);
         }
 
-        var datas = mGraupDatasMap[mAxisType];
+        mRotationParent.transform.rotation = Quaternion.identity;
 
-        foreach (var index in datas[mSelectIndex].indices)
+        foreach(var cube in CreateRotationGroup((int)mAxisType, mSelectIndex))
         {
-            mCubes[index].transform.SetParent(mRotationParent.transform);
+            cube.transform.SetParent(mRotationParent.transform);
         }
+    }
+
+    List<GameObject> CreateRotationGroup(int axisIndex, int selectIndex)
+    {
+        List<GameObject> result = new List<GameObject>();
+
+        foreach(var cube in mCubes)
+        {
+            if (IsGroup(cube, axisIndex, selectIndex))
+            {
+                result.Add(cube);
+            }
+        }
+
+        return result;
+    }
+
+    bool IsGroup(GameObject cube, int axisIndex, int selectIndex)
+    {
+        float t = 0.5f;
+
+        var position = cube.transform.position;
+        float axisValue = position[axisIndex];
+        float select = mComparisonSelectValue[selectIndex];
+
+        float min = select - t;
+        float max = select + t; 
+
+        return min < axisValue && axisValue < max;
+    }
+
+    void CreateComparisonSelectValue()
+    {
+        mComparisonSelectValue = new List<float>(){
+            -1.0f, 0.0f, 1.0f
+        };
     }
 
     private void InputUpdate()
@@ -277,7 +430,35 @@ public class Auto_RubikCube : MonoBehaviour
             }
         }
     }
+
+    private void Factory_RubikCube_Ex()
+    {
+        float OneSize = 1.0f;
+
+        float halfCount = mSize * 0.5f;
+        float halfOneCubeSize = OneSize * 0.5f;
+        float fieldSize = mSize * OneSize;
+
+        var startPosition = CalculateStartPosition_Ex(fieldSize);
+
+        for (int i = 0; i < mSize; ++i)
+        {
+            
+        }
+    }
     
+    private Vector3 CalculateStartPosition_Ex(float fieldSize)
+    {
+        Vector3 position = new Vector3(0, 0, 0);
+        var scale = new Vector3(fieldSize, 0, fieldSize);
+        var halfScale = scale * 0.5f;
+        float x = position.x - halfScale.x;
+        float y = position.y;
+        float z = position.z - halfScale.z;
+
+        return new Vector3 (x, y, z);
+    }
+
     /// <summary>
     /// キューブの生成
     /// </summary>
